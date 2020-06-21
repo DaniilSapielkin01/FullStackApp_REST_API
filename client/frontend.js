9bd3574b4945dc06526f3d602c4dfc551d1e6906
Daniil_Sapielkin <vipdanik98@gmail.com>
2020-06-21 17:32:11 +0300

    first commit

---
+++ client/frontend.js
@@ -0,0 +1,65 @@
+import Vue from "https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js";
+import { request } from "express";
+
+new Vue({
+  el: "#app",
+  data() {
+    return {
+      form: {
+        name: "",
+        value: "",
+      },
+      contacts: [],
+    };
+  },
+  computed: {
+    canCreate() {
+      return this.form.value && this.form.name;
+    },
+  },
+  methods: {
+    async createContact() {
+      const newContact = await request("/api/contacts", "POST", contact);
+
+      // const { ...contact } = this.form;
+      this.contacts.push(newContact);
+
+      this.form.name = this.form.value = "";
+    },
+    async markContact(id) {
+      const contact = this.contacts.find((c) => c.id === id);
+      const updated = await request(`/api/contacts${id}`, "PUT", {
+        ...contact,
+        marked: true,
+      });
+      contact.marked = updated.marked;
+    },
+    async removeContact(id) {
+      await request(`/api/contacts/${id}`, "DELETE");
+      this.contacts = this.contacts.filter((c) => c.id !== id);
+    },
+    async mounted() {
+      this.contacts = await request("/api/contacts");
+    },
+  },
+});
+
+async function request(url, method = "GET", data = null) {
+  try {
+    const headers = {};
+    let body;
+    if (data) {
+      headers["Content-type"] = ["application/json"];
+      body = JSON.stringify(data);
+    }
+
+    const response = await fetch(url, {
+      method: method,
+      headers,
+      body,
+    });
+    return await response.json();
+  } catch (e) {
+    console.warn("Error", e.message);
+  }
+}
